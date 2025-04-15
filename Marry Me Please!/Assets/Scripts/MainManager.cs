f841faaf4986caca54f6721562e9124799f7ba71
Na7han1208 <nathantfouche@gmail.com>
2025-04-15 11:53:48 +0200

    Refactoring of level system and implementation of level manager method in main manager class
    so long unity scenes and hello dynamic active states!

--- Marry Me Please!/Assets/Scripts/MainManager.cs
+++ Marry Me Please!/Assets/Scripts/MainManager.cs
@@ -4,6 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
+    [SerializeField] GameObject ThroneRoom;
+    [SerializeField] GameObject TrainingGrounds;
+    [SerializeField] GameObject Kitchen;
+    [SerializeField] GameObject GreatHall;
+    [SerializeField] GameObject Garden;
+    [SerializeField] GameObject Study;
+    [SerializeField] GameObject Courtyard;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -22,5 +30,38 @@ public class MainManager : MonoBehaviour
 
     public void returnToMenu(){
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void loadBackground(int levelIndex){
+        /*
+            LEVEL INDEX
+            1 - Throne Room
+            2 - Training Grounds
+            3 - Kitchen
+            4 - Great Hall
+            5 - Garden
+            6 - Study
+            7 - Courtyard
+        */
+
+        //Make sure all other rooms are inactive
+        ThroneRoom.SetActive(false);
+        TrainingGrounds.SetActive(false);    
+        Kitchen.SetActive(false);            
+        GreatHall.SetActive(false);          
+        Garden.SetActive(false);             
+        Study.SetActive(false);              
+        Courtyard.SetActive(false);   
+
+        //Load parsed room
+        switch(levelIndex){
+            case 1: ThroneRoom.SetActive(true);         break;
+            case 2: TrainingGrounds.SetActive(true);    break;
+            case 3: Kitchen.SetActive(true);            break;
+            case 4: GreatHall.SetActive(true);          break;
+            case 5: Garden.SetActive(true);             break;
+            case 6: Study.SetActive(true);              break;
+            case 7: Courtyard.SetActive(true);          break;
+        }
     }
 }
