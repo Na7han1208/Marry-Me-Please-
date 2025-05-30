8a93bc27fa2e5e15fa96a03a048f6a3d51e93b36
Na7han1208 <nathantfouche@gmail.com>
2025-05-30 11:53:56 +0200

    Tweaks

--- Marry Me Please!/Assets/Scripts/EndScreenManager.cs
+++ Marry Me Please!/Assets/Scripts/EndScreenManager.cs
@@ -1,20 +1,27 @@
+using System;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 
 public class EndScreenManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text affinityTxt;
+    public GameObject[] characters;
 
     void Start()
     {
         SaveData data = SaveLoadManager.Instance.LoadGame();
-        affinityTxt.text =  "Fen:\t" + data.fenAffinity +
-                            "\nMing:\t" + data.mingAffinity +
-                            "\nJinhui:\t" + data.jinhuiAffinity +
-                            "\nTheo:\t" + data.theodoreAffinity;
-        
+
+        if (data.mingAffinity > 50) characters[0].SetActive(false);
+        if (data.theodoreAffinity > 50) characters[1].SetActive(false);
+        if (data.zihanAffinity > 50) characters[2].SetActive(false);
+        if (data.fenAffinity > 50) characters[3].SetActive(false);
+        if (data.yilinAffinity > 50) characters[4].SetActive(false);
+        if (data.yukiAffinity > 50) characters[5].SetActive(false);
+        if (data.jinhuiAffinity > 50) characters[6].SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -24,5 +31,52 @@ public class EndScreenManager : MonoBehaviour
         {
             SceneManager.LoadScene("MainMenu");
         }
+    }
+
+    private Boolean playerGetsNoBitches(GameObject[] characters)
+    {
+        foreach (GameObject character in characters)
+        {
+            if (character.activeSelf)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void mingEnding()
+    {
+
+    }
+
+    void theoEnding()
+    {
+
+    }
+
+    void zihanEnding()
+    {
+
+    }
+
+    void fenEning()
+    {
+
+    }
+
+    void yilinEnding()
+    {
+
+    }
+
+    void yukiEnding()
+    {
+
+    }
+
+    void jinhuiEnding()
+    {
+        
     }
 }
