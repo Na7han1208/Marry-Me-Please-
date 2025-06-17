b05b8496278c610ff1993a6cee8b510d064045f0
Na7han1208 <125017312+Na7han1208@users.noreply.github.com>
2025-06-17 11:32:59 +0200

    + Scene Transitions & Minigame Transitions

--- Marry Me Please!/Assets/Scripts/MainManager.cs
+++ Marry Me Please!/Assets/Scripts/MainManager.cs
@@ -4,15 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
+    [SerializeField] GameObject BlackScreen;
     [SerializeField] GameObject ThroneRoom;
-    [SerializeField] GameObject TrainingGrounds;
-    [SerializeField] GameObject Kitchen;
-    [SerializeField] GameObject GreatHall;
-    [SerializeField] GameObject Garden;
     [SerializeField] GameObject Study;
-    [SerializeField] GameObject Courtyard;
-    [SerializeField] GameObject Hallway;
-    [SerializeField] GameObject BedChamber;
+    [SerializeField] GameObject Garden;
+
 
     void Update(){
         if(Input.GetKeyDown(KeyCode.Escape)){
@@ -39,27 +35,18 @@ public class MainManager : MonoBehaviour
         */
 
         //Make sure all other rooms are inactive
-        ThroneRoom.SetActive(false);
-        TrainingGrounds.SetActive(false);    
-        Kitchen.SetActive(false);            
-        GreatHall.SetActive(false);          
+
+        BlackScreen.SetActive(false);
+        ThroneRoom.SetActive(false);               
         Garden.SetActive(false);             
         Study.SetActive(false);              
-        Courtyard.SetActive(false);
-        Hallway.SetActive(false);
-        BedChamber.SetActive(false); 
 
         //Load parsed room
         switch(levelIndex){
+            case 0: BlackScreen.SetActive(true);        break;
             case 1: ThroneRoom.SetActive(true);         break;
-            case 2: TrainingGrounds.SetActive(true);    break;
-            case 3: Kitchen.SetActive(true);            break;
-            case 4: GreatHall.SetActive(true);          break;
-            case 5: Garden.SetActive(true);             break;
-            case 6: Study.SetActive(true);              break;
-            case 7: Courtyard.SetActive(true);          break;
-            case 8: Hallway.SetActive(true);            break;
-            case 9: BedChamber.SetActive(true);         break;
+            case 2: Study.SetActive(true);              break;
+            case 3: Garden.SetActive(true);             break;
         }
     }
 }
